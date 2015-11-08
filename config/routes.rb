63ad8be8370f1da58d06965d86d486cbb9308de9
imears <maxwellmears@gmail.com>
2015-11-08 01:30:19 -0800

    Change route to be root

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-
-  # Example of regular route:
-    get 'say-something-smart' => 'test#view'
+  root 'test#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
